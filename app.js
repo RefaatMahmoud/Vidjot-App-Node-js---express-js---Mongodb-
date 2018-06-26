04931c1c2ef55dc705e293a88cd8d1292b9a4d30
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-06-26 02:50:34 -0700

    Index and About Routes

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const port = 3000;
 var app = express();
 
+//Routes
+app.get('/' ,(req , res)=>{
+    res.send('Index');
+});
+
+app.get('/about' ,(req , res)=>{
+    res.send('About');
+});
+
 //Listen server
 app.listen(port , ()=>{
     console.log(`server started with port${port}`)
