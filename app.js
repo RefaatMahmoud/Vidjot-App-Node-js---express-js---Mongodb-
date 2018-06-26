caa921fd2815b9e2092136df4abd01798fb9fb37
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-06-26 07:41:34 -0700

    install handlebars template-engine

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const port = 3000;
 var app = express();
 
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Routes
-app.get('/' ,(req , res)=>{
-    res.send('Index');
+app.get('/', (req, res) => {
+    var author = "Refaat Aish";
+    res.render('Index', {
+        author: author
+    });
 });
 
-app.get('/about' ,(req , res)=>{
-    res.send('About');
+app.get('/about', (req, res) => {
+    res.render('About');
 });
 
 //Listen server
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`server started with port${port}`)
 });
