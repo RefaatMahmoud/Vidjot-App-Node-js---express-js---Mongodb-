257e8ab70a193cc127d4f25e2d6090c675d9491a
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-07-05 08:42:39 -0700

    Fix 12

--- app.js
+++ app.js
@@ -47,11 +47,25 @@ app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 
 //express-session middleware
+/*
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
+*/
+
+var sess = {
+    secret: 'keyboard cat',
+    cookie: {}
+}
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(session(sess));
 
 //passport middleware
 app.use(passport.initialize());
