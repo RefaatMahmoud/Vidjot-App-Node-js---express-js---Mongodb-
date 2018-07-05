0f492401b2a5c7cb6bf99b092aa45a283b462d5d
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2018-07-05 15:27:51 -0700

    Fix issues

--- config/database.js
+++ config/database.js
@@ -1,5 +1,9 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://CHANGEME'}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI: 'mongodb://RefaatAish:Refo10466@ds127811.mlab.com:27811/devjot-prod'
+  }
 } else {
-  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+  module.exports = {
+    mongoURI: 'mongodb://localhost/vidjot-dev'
+  }
 }
